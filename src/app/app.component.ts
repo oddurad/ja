fbd60a0a3752523ec54f0a65d152afb32b006d62
Oddur Aðalgeirsson <oddurad@gmail.com>
2017-10-12 20:36:28 +0000

    [Logo] Moved logo to root template
    The logo is now displayed above every page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
+    <div class="row logo-top">
+      <img class="center-block img-responsive" src="/assets/img/ja.png">
+    </div>
     <div class="container">
       <router-outlet></router-outlet>
     </div>
