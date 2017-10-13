e0dc8126c17cf930633c331c9322f4a6caed319a
Oddur Aðalgeirsson <oddurad@gmail.com>
2017-10-13 11:44:43 +0000

    [Logo] Logo now links to the search page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <div class="row logo-top">
-      <img class="center-block img-responsive" src="/assets/img/ja.png">
+      <a href="/"><img class="center-block img-responsive" src="/assets/img/ja.png"></a>
     </div>
     <div class="container">
       <router-outlet></router-outlet>
